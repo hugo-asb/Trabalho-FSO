94b5d9efd74b3c66fb47a3a7dca614e1e4b56cc6
Hugo Alves <hugo.asb96@gmail.com>
2019-05-07 11:41:26 -0300

    Communicating two process.

---
+++ processo1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mqueue.h>
+#include <sys/stat.h>
+#include <pthread.h>
+
+#define QUEUE "/chat"
+
+mqd_t queue;
+
+void *receive_msg(void *msg){
+  int *a = (int*)msg;
+
+  while(1){
+    if((mq_receive (queue, (void*) &a, sizeof(a), 0)) > 0){
+      perror("mq_open");
+      exit(1);
+    }
+    printf("Received msg: %d \n", *a);
+    pthread_exit(0);
+  }
+}
+
+int main(){
+  int *msg;
+  struct mq_attr attr;
+  pthread_t thread_id;
+
+  msg = (int*)malloc(sizeof(int));
+
+  attr.mq_maxmsg = 10;
+  attr.mq_msgsize = sizeof(msg);
+  attr.mq_flags = 0;
+
+  if ((queue = mq_open (QUEUE, O_RDWR|O_CREAT, 0666, &attr)) < 0){
+    perror ("mq_open");
+    exit (1);
+  }
+
+  pthread_create(&thread_id, NULL, receive_msg, (void*) msg);
+}
